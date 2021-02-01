d64e7326f8bd1a03fb638720f8efcf4096e3cafa
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-02-01 14:17:37 +0100

    Creato il file php
    Creato il file php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/*ISTRUZIONI:
+Riscrivere questa pagina del sito google
+https://policies.google.com/faq. 
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+*/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Php-google-faq</title>
+    </head>
+    <body>
+    
+    </body>
+</html>
